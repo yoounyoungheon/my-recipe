b96284caa1a46f9bf3e2ce9a0cf011826f287420
yoounyoungheon <iddyoon@gmail.com>
2024-10-12 13:47:46 +0900

    home layout

--- web/src/app/(routes)/home/page.tsx
+++ web/src/app/(routes)/home/page.tsx
@@ -1,7 +1,23 @@
 import { lusitana } from "@/app/fonts/fonts"
+import { Card, CardContent, CardFooter } from "@/app/ui/component/molecule/card/card";
 
 export default function Page(){
+  const numbers: number[] = [1,2,3,4,5,6,7,8,8,10];
+  const gridCards: JSX.Element[] = numbers.map((number)=>(
+    <>
+    <Card>
+        <CardContent className="space-y-4 pt-6">
+          <div className="space-y-2">
+          <p>My recipe{number}.</p>
+          </div>
   
+        </CardContent>
+        <CardFooter>
+          <p>This is recipe card footer area</p>
+        </CardFooter>
+      </Card>
+    </>
+  ))
   return (
     <main>
       {/* <div className="pt-6"></div> */}
@@ -9,7 +25,7 @@ export default function Page(){
         Recipe List
       </h1>
       <div className="grid gap-6 sm:grid-rows-2 lg:grid-rows-4">
-        
+        {gridCards}
       </div>
     </main>
   )
