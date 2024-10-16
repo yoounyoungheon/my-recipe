c8ee588e12cf4a71df7dd207d2d60ada4977be7b
yoounyoungheon <iddyoon@gmail.com>
2024-10-16 21:18:44 +0900

    rename modufy-update

--- web/src/app/(routes)/home/update-recipe/page.tsx
+++ web/src/app/(routes)/home/update-recipe/page.tsx
@@ -141,7 +141,7 @@ export default function Page(){
     <main>
       {/* <div className="pt-6"></div> */}
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`} >
-        Modify Recipe: {titleKey} (version: {versionKey})
+        Update Recipe: {titleKey} (version: {versionKey})
       </h1>
       <div className="grid gap-6 sm:grid-rows-2 lg:grid-rows-4">
       <Form 
